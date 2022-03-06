3258756c4be1ac6743e375783c55e6ea4acf80a3
tubotao <tubotao@sugo.io>
2022-03-07 00:55:45 +0800

    feat: img,video,iframe增加懒加载功能

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
-const visit = require("unist-util-visit-parents")
+const visit = require("unist-util-visit-parents");
+const cheerio = require("cheerio");
 
 module.exports = ({ markdownAST }) => {
-  const htmls = []
-  visit(markdownAST, "html", node => {
-    htmls.push(node)
-  })
-  const imageNodes = htmls.filter(html => html.value.indexOf("<img") >= 0)
+  const htmls = [];
+  visit(markdownAST, "html", (node) => {
+    htmls.push(node);
+  });
+  const imageNodes = htmls.filter((html) => html.value.indexOf("<img") >= 0);
+  const videoNodes = htmls.filter((html) => html.value.indexOf("<video") >= 0);
+  const iframeNodes = htmls.filter(
+    (html) => html.value.indexOf("<iframe") >= 0
+  );
 
-  return Promise.all(
-    imageNodes.map(
-      imageNode =>
-        new Promise(resolve => {
+  return Promise.all([
+    ...imageNodes.map(
+      (item) =>
+        new Promise((resolve) => {
+          const $ = cheerio.load(item.value);
+
           // Add class lazyload
-          imageNode.value = imageNode.value.replace(
-            "gatsby-resp-image-image",
-            "gatsby-resp-image-image lazyload"
-          )
+          $("img").addClass("lazy");
+          let html = $.html();
           // Replace src to data-src
-          imageNode.value = imageNode.value.replace("src=", "data-src=")
+          html = html.replace("src=", "data-src=");
           // Replace srcset to data-srcset
-          imageNode.value = imageNode.value.replace("srcset=", "data-srcset=")
+          html = html.replace("srcset=", "data-srcset=");
+          // Replace sizes to data-sizes
+          html = html.replace("sizes=", "data-sizes=");
 
-          resolve(imageNode)
+          // console.log("img", html);
+
+          item.value = html;
+          resolve(item);
         })
-    )
-  )
-}
+    ),
+    ...videoNodes.map(
+      (item) =>
+        new Promise((resolve) => {
+          const $ = cheerio.load(item.value);
 
+          // Add class lazyload
+          $("video").addClass("lazy");
+          let html = $.html();
+          html = html.replace("poster=", "data-poster=");
+          html = html.replace("src=", "data-src=");
+
+          // console.log("video", html);
+
+          item.value = html;
+          resolve(item);
+        })
+    ),
+    ...iframeNodes.map(
+      (item) =>
+        new Promise((resolve) => {
+          const $ = cheerio.load(item.value);
+
+          $("iframe").addClass("lazy");
+          let html = $.html();
+          // Replace src to data-src
+          html = html.replace("src=", "data-src=");
+
+          // console.log("iframe", html);
+
+          item.value = html;
+          resolve(item);
+        })
+    ),
+  ]);
+};
